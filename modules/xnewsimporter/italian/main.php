2271c89070104234e47a35fbcf0e97991e6d5244
Defkon1 <defkon1@gmail.com>
2011-04-08 16:32:52 +0200

    xnewsimporter fix

--- modules/xnewsimporter/italian/main.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("_MA_XNI_NAME","Nome");
-define("_MA_XNI_EMAIL","Email");
-define("_MA_XNI_ADDRESS","Indirizzo");
-define("_MA_XNI_TELEPHONE","Telefono");
-?>
--- modules/xnewsimporter/italian/modinfo.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("_MI_XNI_CONFIGURE" , "Gestione importazione");
-define("_MI_XNI_PREFERENCES" , "Preferenze");
-define("_MI_XNI_IMPORT" , "Importa");
-define("_MI_XNI_STORYCOUNTADMIN", "Articoli e argomenti da mostrare in amministrazione");
-define("_MI_XNI_STORYCOUNTADMIN_DESC", "Questa opzione verr&agrave; utilizzata per limitare il numero di articoli e topic mostrati<br /> in amministrazione e nelle statistiche.");
-?>
