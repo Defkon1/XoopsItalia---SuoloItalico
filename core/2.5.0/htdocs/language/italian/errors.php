d5f484f5851348c2a1087a30d46c277ea93b96cf
Defkon1 <defkon1@gmail.com>
2011-04-08 13:04:23 +0200

    core 2.5.0 - errors.php - strings added (thx to timgno)

--- core/2.5.0/htdocs/language/italian/errors.php
+++ core/2.5.0/htdocs/language/italian/errors.php
@@ -6,4 +6,18 @@
 define('_XO_ER_FILENOTFOUND', 'Il file richiesto: <b>%s</b> non &egrave; stato trovato ');
 define('_XO_ER_CLASSNOTFOUND', 'La classe richiesta %s non &egrave; stata trovata');
 
+// Error Pages by TXMod Xoops
+define('_XO_ER_TITLE', 'Errore non classificato (Not Classified)'); 
+define('_XO_ER_DESC', 'Siamo spiacenti, ma questo errore non è stato classificato.');
+define('_XO_ER_TITLE400', 'Errore 400 - Richiesta errata (Bad Request)'); 
+define('_XO_ER_DESC400', 'Siamo spiacenti, ma è stata fatta una richiesta errata.');
+define('_XO_ER_TITLE401', 'Errore 401 - Necessaria autorizzazione (Authorization required)');
+define('_XO_ER_DESC401', 'Siamo spiacenti, ma l&#39;accesso alla pagina richiesta &#232; proibito, necessaria autorizzazione.');
+define('_XO_ER_TITLE403', 'Errore 403 - Accesso negato (Forbidden)'); 
+define('_XO_ER_DESC403', 'Siamo spiacenti, ma l&#39;accesso a questa pagina è stato negato.');
+define('_XO_ER_TITLE404', 'Errore 404 - Pagina non trovata (Not found)'); 
+define('_XO_ER_DESC404', 'Siamo spiacenti, ma la pagina cercata è inesistente o &#232; stata rimossa.');
+define('_XO_ER_TITLE500', 'Errore 500 - Errore interno del server (Internal server error)');
+define('_XO_ER_DESC500', 'Siamo spiacenti, ma il server in questo momento non risponde a causa di un errore interno.');
+
 ?>
